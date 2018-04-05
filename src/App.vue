63d51599bceecc8d9dc3812142e7a26c806d4e32
Grishin Anton <grishin_anton@mail.ru>
2018-04-05 16:29:59 +0300

    Первичная настройка. Подключние pug ans scss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    router-view
 </template>
 
 <script>
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +18,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  
+}
+
+.hello {
+  background: #000;
+
+  h1 {
+    color: red;
+  }
 }
 </style>
